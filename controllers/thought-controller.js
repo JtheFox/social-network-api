6c65716d5355c7dc0f854c99ec3aecf757a1f1d3
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 13:01:16 -0400

    add thought functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,37 @@
 import { User, Thought } from '../models/index.js';
 
 const thoughtController = {
-    
-}
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            !thoughts.length ? res.status(404).json({ message: 'No thoughts found' }) : res.json(thoughts);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async getOneThought({ params }, res) {
+        try {
+            const thought = await Thought.findOne({ _id: params.thoughtId });
+            !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async createThought({ body }, res) {
+        try {
+            const thought = await Thought.create(body);
+            const user = User.findOneAndUpdate({ username: body.username }, { $addToSet: { thoughts: thought._id } }, { new: true })
+            !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async updateThought({params, body}, res) {
+
+    }
+ }
 
 export default thoughtController;
