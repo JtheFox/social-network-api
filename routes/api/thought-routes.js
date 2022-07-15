d7876e84d4e70872ef69ccf2698c3dad2d31e29f
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 13:56:16 -0400

    create routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,6 +3,17 @@ import Thoughts from '../../controllers/thought-controller.js';
 
 const router = Router();
 
+router.route('/')
+    .get(Thoughts.getAllThoughts)
+    .post(Thoughts.createThought);
 
+router.route('/:thoughtId')
+    .get(Thoughts.getOneThought)
+    .put(Thoughts.updateThought)
+    .delete(Thoughts.deleteThought);
+
+router.route('/:thoughtId/reactions/:reactionId?')
+    .post(Thoughts.addReaction)
+    .delete(Thoughts.removeReaction);
 
 export default router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,6 +3,17 @@ import Users from '../../controllers/user-controller.js';
 
 const router = Router();
 
+router.route('/')
+    .get(Users.getAllUsers)
+    .post(Users.createUser);
 
+router.route('/:userId')
+    .get(Users.getOneUser)
+    .put(Users.updateUser)
+    .delete(Users.deleteUser);
+
+router.route('/:userId/friends/:friendId')
+    .post(Users.addToFriends)
+    .delete(Users.removeFromFriends);
 
 export default router;
