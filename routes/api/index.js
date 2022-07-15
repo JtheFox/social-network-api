ecf65050a710a851d14d5b91f9b1a59fd7230c04
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 11:54:25 -0400

    refactor

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-const router = Router();
 import userRoutes from './user-routes.js';
 import thoughtRoutes from './thought-routes.js';
+
+const router = Router();
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
