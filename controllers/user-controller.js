45e3c66906e4e8fa2fd6327b3e8fd848a4b2ec3b
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 12:48:46 -0400

    update user functions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -30,7 +30,7 @@ const userController = {
     },
     async updateUser({ params, body }, res) {
         try {
-            const user = await User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true });
+            const user = await User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true });
             !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user);
         } catch (err) {
             console.log(err);
@@ -39,12 +39,36 @@ const userController = {
     },
     async deleteUser({ params, body }, res) {
         try {
-           
+            const user = await User.deleteOne({ _id: params.userId });
+            if (!user) res.status(404).json({ message: 'No user with that ID' });
+            else {
+                await User.updateMany({ _id: { $in: user.friends } }, { $pull: { friends: params.userId } });
+                await Thought.deleteMany({ username: user.username });
+                res.json({ message: `User ${user.username} deleted successfully` });
+            }
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
         }
     },
+    async addToFriends({ params }, res) {
+        try {
+            const user = await User.updateOne({ _id: params.userId }, { $push: { friends: params.friendId } }, { new: true });
+            !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async removeFromFriends({ params }, res) {
+        try {
+            const user = await User.updateOne({ _id: params.userId }, { $pull: { friends: params.friendId } }, { new: true });
+            !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
 }
 
 export default userController;
