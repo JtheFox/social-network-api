dc89478da611ad177c5677e4eeec56cbf727821c
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 11:11:56 -0400

    add & create models

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose';
+import moment from 'moment';
+
+const Thought = model('Thought', new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [new Schema({
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')
+        }
+    }, {
+        toJSON: { virtuals: true }
+    })]
+}, {
+    toJSON: { virtuals: true }
+}).virtual('reactionCount').get(() => this.reactions.length));
+
+export default { Thought };
---
+++ models/User.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const User = model('User', new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: { virtuals: true }
+}).virtual('friendCount').get(() => this.friends.length));
+
+export default { User };
