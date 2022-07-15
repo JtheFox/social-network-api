8cb9344c6de68e29b5909887e09cbce1ecfe096e
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 11:45:57 -0400

    add server start

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ import routes from './routes';
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.json())
-    .use(express.urlencoded({ extended: true }));
+app
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`API server running on localhost:${PORT}`));
+});
