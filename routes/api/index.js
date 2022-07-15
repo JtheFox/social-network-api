bdcc8d5bb7c7a0986a85fc9579031a561184890d
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-15 11:49:58 -0400

    fix issues with es modules

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
-import userRoutes from './user-routes';
-import thoughtRoutes from './thought-routes';
+import userRoutes from './user-routes.js';
+import thoughtRoutes from './thought-routes.js';
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
